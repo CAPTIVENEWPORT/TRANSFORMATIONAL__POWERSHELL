f0a3e8555e0f8f7e536e6edbadde5f6178213f72
CAPTIVENEWPORT <71532467+CAPTIVENEWPORT@users.noreply.github.com>
2020-11-13 00:30:18 -0800

    Add files via upload
    ONE OF THE MOST HELPFUL OF THESE SCRIPTS IS THE ACL_OWNERSHIP.PS1 CODE. 
    
    THIS ALLOWS YOU TO OVERRIDE THE OWNERSHIP OF THE BUILT-IN WINDOWS NTAUTHORITY\ACCOUNT

---
+++ POWERSHELL_REFINED/ACL_OWNERSHIP.ps1
@@ -0,0 +1,5 @@
+﻿$ACL = Get-Acl -Path "*"
+$User = New-Object System.Security.Principal.NTAccount("UTLUSR")
+$ACL.SetOwner($User)
+$ACL | Set-Acl -Path "*"
+Get-Acl -Path "*" -AllCentralAccessPolicies | Format-Custom * -Force | Out-File .\Users\UTLUSR\CAPTIVE_DEV\POWERSHELL_REFINED\COPYING_ACL_PERMISSIONS_OUTPUT.LOG  -Append ascii -Force
